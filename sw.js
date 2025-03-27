01f2aca7bfeda0e57ec7f046fe7d69fad9b17667
xp <libaiunique123@gmail.com>
2025-03-27 21:46:09 +0800

    win pwa test

--- sw.js
+++ sw.js
@@ -6,6 +6,28 @@ const urlsToCache = [
   'https://cdn.jsdelivr.net/npm/iztro/dist/iztro.min.js'
 ];
 
+
+// Listen to the widgetinstall event.
+self.addEventListener("widgetinstall", event => {
+  // The widget just got installed, render it using renderWidget.
+  // Pass the event.widget object to the function.
+  event.waitUntil(renderWidget(event.widget));
+});
+
+async function renderWidget(widget) {
+  // Get the template and data URLs from the widget definition.
+  const templateUrl = widget.definition.msAcTemplate;
+  const dataUrl = widget.definition.data;
+
+  // Fetch the template text and data.
+  const template = await (await fetch(templateUrl)).text();
+  const data = await (await fetch(dataUrl)).text();
+
+  // Render the widget with the template and data.
+  await self.widgets.updateByTag(widget.definition.tag, {template, data});
+}
+
+
 // 安装Service Worker并缓存资源
 self.addEventListener('install', event => {
   event.waitUntil(
