6986559a5d7547f2a7f98aa77b1af8758812f096
“xupei” <310715321@qq.com>
2025-04-01 18:36:56 +0800

    111

--- sw-widgets.js
+++ sw-widgets.js
@@ -1,29 +1,29 @@
 const WIDGET_TAG = 'pwamp';
 const HELLO_WIDGET_TAG = 'hello-world';
 
-// Listen to the widgetinstall event.
+// 监听widgetinstall事件
 self.addEventListener("widgetinstall", event => {
-  // The widget just got installed, render it using renderWidget.
-  // Pass the event.widget object to the function.
+  // 小组件刚刚安装，使用renderWidget渲染它
+  // 将event.widget对象传递给函数
   event.waitUntil(renderWidget(event.widget));
 });
 
 async function renderWidget(widget) {
-  // Get the template and data URLs from the widget definition.
+  // 从小组件定义中获取模板和数据URL
   const templateUrl = widget.definition.msAcTemplate;
   const dataUrl = widget.definition.data;
 
-  // Fetch the template text and data.
+  // 获取模板文本和数据
   const template = await (await fetch(templateUrl)).text();
   const data = await (await fetch(dataUrl)).text();
 
-  // Render the widget with the template and data.
+  // 使用模板和数据渲染小组件
   await self.widgets.updateByTag(widget.definition.tag, {template, data});
 }
 
 
-// Update the widgets to their initial states
-// when the service worker is activated.
+// 当服务工作线程被激活时，
+// 将小组件更新到初始状态
 self.addEventListener("activate", event => {
   event.waitUntil(updateWidgets());
 });
@@ -36,17 +36,17 @@ async function updateWidgets() {
 }
 
 async function updatePwampWidget() {
-  // Get the widget that match the tag defined in the web app manifest.
+  // 获取与Web应用清单中定义的标签匹配的小组件
   const widget = await self.widgets.getByTag(WIDGET_TAG);
   if (!widget) {
     return;
   }
 
-  // Using the widget definition, get the template and data.
+  // 使用小组件定义，获取模板和数据
   const template = await (await fetch(widget.definition.msAcTemplate)).text();
   const data = await (await fetch(widget.definition.data)).text();
 
-  // Render the widget with the template and data.
+  // 使用模板和数据渲染小组件
   await self.widgets.updateByTag(widget.definition.tag, {template, data});
 }
 
@@ -68,10 +68,10 @@ async function updateHelloWorldWidget() {
 self.addEventListener('widgetclick', (event) => {
   switch (event.action) {
     case 'previous-song':
-      // Application logic to play the previous song...
+      // 播放上一首歌的应用逻辑...
       break;
     case 'next-song':
-      // Application logic to play the next song...
+      // 播放下一首歌的应用逻辑...
       break;
   }
 });
