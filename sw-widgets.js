3ca9834efe0821ffff2cb6ca779529572ab984ca
xp <libaiunique123@gmail.com>
2025-04-08 01:27:26 +0800

    1

--- sw-widgets.js
+++ sw-widgets.js
@@ -37,15 +37,6 @@ async function renderWidget(widget) {
   await self.widgets.updateByTag(widget.definition.tag, {template, data});
 }
 
-
-// 当服务工作线程被激活时，
-// 将小组件更新到初始状态
-self.addEventListener("activate", event => {
-  event.waitUntil(updateWidgets());
-  // 设置定期更新小组件
-});
-
-
 async function onWidgetUninstall(widget) {
   //卸载时，注销周期同步。
   //如果这是最后一个小部件实例，那么注销周期性同步。
@@ -63,22 +54,34 @@ function replaceDatePlaceholders(dataString) {
   return dataString.replace(/"2000-00-00T00:00:00Z"/g, `"${now}"`);
 }
 
+// 当服务工作线程被激活时，
+// 将小组件更新到初始状态
+self.addEventListener("activate", event => {
+  event.waitUntil(updateWidgets());
+  // 设置定期更新小组件
+  setInterval(updateWidgets, 1000); // 每分钟更新一次
+});
 
-async function updateWidgets() {
-  // Get the widget that match the tag defined in the web app manifest.
+async function updateWidgets(event) {
+  // 更新hello小组件
+  await updatePwampWidget();
+}
+
+async function updatePwampWidget() {
+  // 获取与Web应用清单中定义的标签匹配的小组件
   const widget = await self.widgets.getByTag(WIDGET_TAG);
   if (!widget) {
     return;
   }
 
-  // 从小组件定义中获取模板和数据URL
-  const templateUrl = widget.definition.msAcTemplate;
-  const dataUrl = widget.definition.data;
-  // 获取模板文本和数据
-  const template = await (await fetch(templateUrl)).text();
-  let data = await (await fetch(dataUrl)).text();
+  // 使用小组件定义，获取模板和数据
+  const template = await (await fetch(widget.definition.msAcTemplate)).text();
+  let data = await (await fetch(widget.definition.data)).text();
+
   // 替换数据中的日期占位符
   data = replaceDatePlaceholders(data);
+
   // 使用模板和数据渲染小组件
   await self.widgets.updateByTag(widget.definition.tag, {template, data});
 }
+
