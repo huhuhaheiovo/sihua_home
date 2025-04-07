ae7e9c82986a3900b5f1dbb13d9edd2cffc4ac05
xp <libaiunique123@gmail.com>
2025-04-08 00:49:38 +0800

    1

--- sw-widgets.js
+++ sw-widgets.js
@@ -25,22 +25,8 @@ async function renderWidget(widget) {
 }
 
 
-async function onWidgetUninstall(widget) {
-  // On uninstall, unregister the periodic sync.
-  // If this was the last widget instance, then unregister the periodic sync.
-  if (widget.instances.length === 1 && "update" in widget.definition) {
-    await self.registration.periodicSync.unregister(widget.definition.tag);
-  }
-}
 
-// Listen to periodicsync events to update all widget instances
-// periodically.
-self.addEventListener("periodicsync", async event => {
-  const widget = await self.widgets.getByTag(event.tag);
-  if (widget && "update" in widget.definition) {
-    event.waitUntil(renderWidget(widget));
-  }
-});
+
 
 
 
@@ -55,27 +41,20 @@ function replaceDatePlaceholders(dataString) {
 // 当服务工作线程被激活时，
 // 将小组件更新到初始状态
 self.addEventListener("activate", event => {
-  // event.waitUntil(updateWidgets());
+  event.waitUntil(updateWidgets());
   // 设置定期更新小组件
-  setInterval(updateWidgets, 5000);
+  // setInterval(updateWidgets, 5000);
 });
 
-async function updateWidgets(event) {
-  // 更新hello小组件
-  await updatePwampWidget();
-}
-
-async function updatePwampWidget() {
-  // 获取与Web应用清单中定义的标签匹配的小组件
+async function updateWidgets() {
   const widget = await self.widgets.getByTag(WIDGET_TAG);
   if (!widget) {
     return;
   }
-
   // 使用小组件定义，获取模板和数据
   const template = await (await fetch(widget.definition.msAcTemplate)).text();
   let data = await (await fetch(widget.definition.data)).text();
-  
+
   // 替换数据中的日期占位符
   data = replaceDatePlaceholders(data);
 
