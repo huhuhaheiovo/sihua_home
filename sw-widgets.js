84781d1f074197c5598d01c71043d25d22976e85
xp <libaiunique123@gmail.com>
2025-04-08 01:22:46 +0800

    1

--- sw-widgets.js
+++ sw-widgets.js
@@ -8,7 +8,14 @@ const WIDGET_TAG = 'hello';
 self.addEventListener("activate", event => {
   event.waitUntil(updateWidgets());
   // 设置定期更新小组件
-  setInterval(updateWidgets, 1000);
+  setInterval(async () => {
+    // 获取所有注册的小组件
+    const widgets = await self.widgets.getAll();
+    // 更新每个小组件
+    for (const widget of widgets) {
+      await renderWidget(widget);
+    }
+  }, 1000);
 });
 
 
