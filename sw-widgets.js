96c2a2955621e6b03ffca1c218629c01a706f6a2
xp <libaiunique123@gmail.com>
2025-04-07 21:51:19 +0800

    1

--- sw-widgets.js
+++ sw-widgets.js
@@ -1,5 +1,4 @@
-const WIDGET_TAG = 'pwamp';
-const HELLO_WIDGET_TAG = 'hello-world';
+const WIDGET_TAG = 'hello';
 
 // 监听widgetinstall事件
 self.addEventListener("widgetinstall", event => {
@@ -12,14 +11,11 @@ async function renderWidget(widget) {
   // 从小组件定义中获取模板和数据URL
   const templateUrl = widget.definition.msAcTemplate;
   const dataUrl = widget.definition.data;
-
   // 获取模板文本和数据
   const template = await (await fetch(templateUrl)).text();
   let data = await (await fetch(dataUrl)).text();
-
   // 替换数据中的日期占位符
   data = replaceDatePlaceholders(data);
-
   // 使用模板和数据渲染小组件
   await self.widgets.updateByTag(widget.definition.tag, {template, data});
 }
@@ -29,32 +25,20 @@ function replaceDatePlaceholders(dataString) {
   // 创建当前日期对象
   const now = new Date();
   // 替换数据中的占位符
-  let updatedData = dataString.replace(/"2000-00-00T00:00:00Z"/g, `"${now}"`);
-  // updatedData = updatedData.replace(/"2000-00-00T00:00:00Z"/g, `"${formattedDate}"`);
-  return updatedData;
+  return dataString.replace(/"2000-00-00T00:00:00Z"/g, `"${now}"`);
 }
 
 // 当服务工作线程被激活时，
 // 将小组件更新到初始状态
 self.addEventListener("activate", event => {
   event.waitUntil(updateWidgets());
-  
-  // 设置定期更新小组件 - 每10秒更新一次
-  setInterval(async () => {
-    // 获取所有注册的小组件
-    const widgets = await self.widgets.getAll();
-    // 更新每个小组件
-    for (const widget of widgets) {
-      await renderWidget(widget);
-    }
-  }, 10000);
+  // 设置定期更新小组件
+  setInterval(updateWidgets, 1000); // 每分钟更新一次
 });
 
-async function updateWidgets() {
-  // 更新pwamp小组件
+async function updateWidgets(event) {
+  // 更新hello小组件
   await updatePwampWidget();
-  // 更新hello-world小组件
-  await updateHelloWorldWidget();
 }
 
 async function updatePwampWidget() {
@@ -75,31 +59,3 @@ async function updatePwampWidget() {
   await self.widgets.updateByTag(widget.definition.tag, {template, data});
 }
 
-async function updateHelloWorldWidget() {
-  // 获取hello-world小组件
-  const widget = await self.widgets.getByTag(HELLO_WIDGET_TAG);
-  if (!widget) {
-    return;
-  }
-
-  // 获取模板和数据
-  const template = await (await fetch(widget.definition.msAcTemplate)).text();
-  let data = await (await fetch(widget.definition.data)).text();
-  
-  // 替换数据中的日期占位符
-  data = replaceDatePlaceholders(data);
-
-  // 渲染小组件
-  await self.widgets.updateByTag(widget.definition.tag, {template, data});
-}
-
-self.addEventListener('widgetclick', (event) => {
-  switch (event.action) {
-    case 'previous-song':
-      // 播放上一首歌的应用逻辑...
-      break;
-    case 'next-song':
-      // 播放下一首歌的应用逻辑...
-      break;
-  }
-});
