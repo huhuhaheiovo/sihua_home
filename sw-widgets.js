839decb461b20a20e1adeb6497deaa53344610e9
“xupei” <310715321@qq.com>
2025-04-08 12:48:14 +0800

    1

--- sw-widgets.js
+++ sw-widgets.js
@@ -36,7 +36,7 @@ async function renderWidget(widget) {
     "week": 1,
     "season_type": "regular",
     "formattedDate": "",
-    "implement": "生命周期",
+    "implement": "widgetuninstall",
     "neutral_site": false,
     "conference_game": false,
     "attendance": null,
@@ -67,7 +67,7 @@ async function renderWidget(widget) {
   try {
     await self.widgets.updateByTag(widget.definition.tag, {
       template: JSON.stringify(template),
-      data: JSON.stringify(initialData)
+      data:initialData
     });
   } catch (e) {
     console.log('Failed to update widget', e);
