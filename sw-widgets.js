3330c4998614c9cae662e465492e799a7a44c6d2
“xupei” <310715321@qq.com>
2025-04-08 13:58:29 +0800

    1

--- sw-widgets.js
+++ sw-widgets.js
@@ -1,7 +1,6 @@
 // 监听widgetinstall事件
 self.addEventListener("widgetinstall", event => {
   // 小组件刚刚安装，使用renderWidget渲染它
-  // 将event.widget对象传递给函数
   event.waitUntil(renderWidget(event.widget));
 });
 
@@ -15,6 +14,43 @@ async function renderWidget(widget) {
   const data = await (await fetch(dataUrl)).json();
   let dataJson=data;
   dataJson.implement="widgetinstall1";
+  // Render the widget with the template and data.
+  try {
+    await self.widgets.updateByTag(widget.definition.tag, {
+      template: JSON.stringify(template),
+      data: JSON.stringify(dataJson)
+    });
+  } catch (e) {
+    console.log('Failed to update widget', e);
+  }
+}
+
+
+
+
+
+// Update the widgets to their initial states
+// when the service worker is activated.
+self.addEventListener("activate", event => {
+  event.waitUntil(updateWidgets());
+});
+
+async function updateWidgets() {
+  // Get the widget that match the tag defined in the web app manifest.
+  const widget = await self.widgets.getByTag("hello");
+  if (!widget) {
+    return;
+  }
+
+  // 从小组件定义中获取模板和数据URL
+  const templateUrl = widget.definition.msAcTemplate;
+  const dataUrl = widget.definition.data;
+
+  // Fetch the template text and data.
+  const template = await (await fetch(templateUrl)).json();
+  const data = await (await fetch(dataUrl)).json();
+  let dataJson=data;
+  dataJson.implement="activate";
   // Render the widget with the template and data.
   try {
     await self.widgets.updateByTag(widget.definition.tag, {
